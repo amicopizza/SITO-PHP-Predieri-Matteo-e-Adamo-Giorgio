8e8e094dadb8fb0c67757c676d1dcfad72704214
Matteo Predieri <58475013+amicopizza@users.noreply.github.com>
2022-05-15 19:59:57 +0200

    Add files via upload

---
+++ fudo maki/admin/manage-admin.php
@@ -0,0 +1,7 @@
+<?php include('parials/menu.php'); ?>
+    <div class="main-content">
+      <div class="wrapper">
+        <h1>Manage Admin</h1>
+      </div>
+    </div>
+<?php include('parials/footer.php'); ?>
---
+++ fudo maki/admin/partials/menu.php
@@ -0,0 +1,16 @@
+<html>
+  <head>
+      <title>Ristorante GPizza - Home Page</title>
+  </head>
+  <body>
+    <div class="menu text-center">
+      <div class="wrapper">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="manage-admin.php">Admin</a></li>
+          <li><a href="manage-category.php">Category</a></li>
+          <li><a href="manage-food.php">Food</a></li>
+          <li><a href="manage-order.php">Order</a></li>
+        </ul>
+      </div>
+    </div>
